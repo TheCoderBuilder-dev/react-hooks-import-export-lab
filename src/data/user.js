ab412630abba328cf630c3ed1580f04501b5ecd1
TheCoderBuilder-dev <brianinesh@gmail.com>
2025-04-13 16:12:47 +0300

    k

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,6 @@
-const username = "Liza";
-const city = "New York";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+const username = "Liza";  // Change from "Brian" to "Liza"
+const city = "New York";  // Change from "Nairobi" to "New York"
+const image = "https://i.imgur.com/mV8PQxj.gif";  // Keep as is, unless test specifies otherwise
 
-// export all three variables as named exports
+export { username, city, image };
